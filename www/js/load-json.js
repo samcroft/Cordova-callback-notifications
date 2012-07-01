dbe691d2ba5440244eac2a1ff7f2eb0e0fb3c9c5
Sam Croft <samcroft@gmail.com>
2012-07-01 19:34:53 +0100

    Confirmation notification and callback function

--- www/js/load-json.js
+++ www/js/load-json.js
@@ -1,27 +1,44 @@
 $(document).ready(function(){
 	$(document).bind('deviceready', function(){
-		//Phonegap ready
 		onDeviceReady();
 	});
-
-	var output = $('#output');
-
-	$.ajax({
-		url: 'http://samcroft.co.uk/demos/updated-load-data-into-phonegap/landmarks.php',
-		dataType: 'jsonp',
-		jsonp: 'jsoncallback',
-		timeout: 5000,
-		success: function(data, status){
-			$.each(data, function(i,item){ 
-				var landmark = '<h1>'+item.name+'</h1>'
-				+ '<p>'+item.latitude+'<br>'
-				+ item.longitude+'</p>';
-			
-				output.append(landmark);
-			});
-		},
-		error: function(){
-		   output.text('There was an error loading the data.');
+	
+	function yourCallback(button) {
+		if (button == 2) {
+			dataRequest();
 		}
-	});
+	}
+	
+	function dataRequest() {
+		var output = $('#output').text('Loading data...');
+
+		$.ajax({
+			url: 'http://samcroft.co.uk/demos/updated-load-data-into-phonegap/landmarks.php',
+			dataType: 'jsonp',
+			jsonp: 'jsoncallback',
+			timeout: 5000,
+			success: function(data, status){
+				output.empty();
+				
+				$.each(data, function(i,item){ 
+					var landmark = '<h1>'+item.name+'</h1>'
+					+ '<p>'+item.latitude+'<br>'
+					+ item.longitude+'</p>';
+
+					output.append(landmark);
+				});
+			},
+			error: function(){
+				output.text('There was an error loading the data.');
+				navigator.notification.confirm(
+					'Something went wrong. Would you like to retry?',
+					yourCallback,
+					'Error',
+					'No,Yes'
+				);
+			}
+		});
+	}
+	
+	dataRequest();
 });
